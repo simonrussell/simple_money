b3d0eb30e98a3a82547ae99b7a7c9c39fe66678f
Simon Russell <simon@bellyphant.com>
2009-11-03 21:48:04 +1100

    Basic operators for Money.

--- lib/localise/money.rb
+++ lib/localise/money.rb
@@ -1,6 +1,7 @@
 module Localise
   class Money
-   
+    include Comparable
+    
     attr_reader :currency, :amount
     
     def initialize(currency, amount)
@@ -11,6 +12,51 @@ module Localise
     def amount_in_decimal
       @currency.format_to_decimal(@amount)
     end
-            
+     
+    def to_s
+      @currency.format_value(@amount, :iso)
+    end
+    
+    def <=>(other)
+      if other.eql?(0)    # it's exactly the integer zero
+        @amount <=> 0
+      elsif compatible_with?(other)
+        @amount <=> other.amount
+      else
+        raise "can't compare Money with #{other}"
+      end
+    end
+    
+    def compatible_with?(other)
+      other.is_a?(Money) && other.currency == @currency
+    end
+    
+    def *(other)
+      raise "can't multiply with #{other}" unless other.is_a?(Numeric)
+      
+      Money.new(@currency, (@amount * other).to_i)
+    end
+    
+    def +(other)
+      if other.eql?(0)
+        self
+      elsif compatible_with?(other)
+        Money.new(@currency, @amount + other.amount)
+      else
+        raise "can't add with #{other}"
+      end
+    end
+    
+    def -(other)
+      if other.eql?(0)
+        self
+      elsif compatible_with?(other)
+        Money.new(@currency, @amount - other.amount)
+      else
+        raise "can't subtract with #{other}"
+      end
+    end
+    
+         
   end
 end
--- spec/lib/localise/money_spec.rb
+++ spec/lib/localise/money_spec.rb
@@ -15,17 +15,60 @@ describe Localise::Money do
     its(:currency) { should == @currency }
     its(:amount) { should == 123 }
     its(:amount_in_decimal) { should == "1.23" }
+    
+    it "should have iso-formatted to_s" do
+      subject.to_s.should == subject.currency.format_value(subject.amount, :iso)
+    end
   end
   
   describe "operators" do
     before do
       @a = subject.new(:XYZ, 1000)
       @b = subject.new(:XYZ, 500)
+      @c = subject.new(:XYZ, 0)
     end
 
-#    it "should support addition" do
-#      (@a + @b).should == subject.new(:XYZ, @a.amount + @b.amount)
-#    end
+    it "should support equality with other money" do
+      @a.should == subject.new(@a.currency, @a.amount)
+    end
+    
+    it "should support equality with zero" do
+      @c.should == 0
+    end
+
+    describe "addition" do
+      it "should support Money" do
+        (@a + @b).should == subject.new(:XYZ, @a.amount + @b.amount)
+      end
+  
+      it "should support 0" do
+        (@a + 0).should == @a
+      end
+      
+      it "should not allow random other stuff" do
+        lambda { @a + "hello" }.should raise_error
+      end 
+    end
+        
+    describe "subtraction" do
+      it "should support Money" do
+        (@a - @b).should == subject.new(:XYZ, @a.amount - @b.amount)
+      end
+  
+      it "should support 0" do
+        (@a - 0).should == @a
+      end
+      
+      it "should not allow random other stuff" do
+        lambda { @a - "hello" }.should raise_error
+      end 
+    end
+        
+    it "should support multiplication with Numeric" do
+      (@b * 2).should == @a
+      (@b * 2.0).should == @a
+    end
+
   end
   
 end
